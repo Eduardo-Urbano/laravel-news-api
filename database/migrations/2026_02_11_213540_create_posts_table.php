9bdd3ceb0a9edf3279c10374774fcc231acb0c5d
Eduardo Urbano <eduardosurbano004@gmail.com>
2026-02-11 18:39:16 -0300

    feat: ajusta migrations de categories e posts

--- database/migrations/2026_02_11_213540_create_posts_table.php
+++ database/migrations/2026_02_11_213540_create_posts_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+
+            $table->string('title');
+            $table->string('tag')->nullable();
+            $table->string('summary');
+            $table->text('content');
+
             $table->timestamps();
         });
     }
